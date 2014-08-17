820a24cce2a0cca7da3b777301da30f216c23c63
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-08-16 20:40:23 -0500

    Create categories and Get All Categories implementing Json Marshaller with testing

--- src/models/categories_test.go
+++ src/models/categories_test.go
@@ -1,1 +1,12 @@
 package models
+
+import (
+        "testing"
+)
+
+func TestGetCategories(t *testing.T) {
+        categories := GetCategories()
+        if len(categories) == 0 {
+                t.Fatal("Categories should not be empty")
+        }
+}
--- src/models/category.go
+++ src/models/category.go
@@ -1,10 +1,63 @@
 package models
 
+import (
+        . "db"
+        "fmt"
+)
+
 type Category struct {
         Id                int8
         Name, Description string
 }
 
 func (c *Category) Create() {
+        db, err := StablishConnection()
+        if err != nil {
+                panic(err)
+        }
+        defer db.Close()
 
+        query := `INSERT INTO categories(id, name, description)
+      VALUES ($1, $2, $3)`
+
+        _, err = db.Exec(query,
+                c.Id,
+                c.Name,
+                c.Description)
+
+        if err != nil {
+                panic(err)
+        }
+}
+
+func GetCategories() []*Category {
+        db, err := StablishConnection()
+        if err != nil {
+                panic(err)
+        }
+        defer db.Close()
+
+        query := `SELECT name FROM categories`
+        categories_rows, err := db.Query(query)
+        if err != nil {
+                panic(err)
+        }
+
+        if categories_rows == nil {
+                panic(categories_rows)
+        }
+
+        categories := []*Category{}
+        for categories_rows.Next() {
+                category := Category{}
+                err = categories_rows.Scan(&category.Name)
+                categories = append(categories, &category)
+        }
+
+        return categories
+}
+
+func (c *Category) MarshalJSON() ([]byte, error) {
+        str := fmt.Sprintf(`{"name": "%s", "description": "%s"}`, c.Name, c.Description)
+        return []byte(str), nil
 }
