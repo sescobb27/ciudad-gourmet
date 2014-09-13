4cc3bf20759fce40b0664b6fb7296a3f32936cda
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-09-13 01:18:22 -0500

    Integration Testing (Testing Hanlders with DB access)

--- handlers/hanlders_test.go
+++ handlers/hanlders_test.go
@@ -1,1 +1,29 @@
 package handlers
+
+import (
+        // . "github.com/smartystreets/goconvey/convey"
+        "github.com/stretchr/testify/assert"
+        "net/http"
+        "net/http/httptest"
+        "testing"
+)
+
+func TestGetLocations(t *testing.T) {
+        t.Parallel()
+        recorder := httptest.NewRecorder()
+        req, err := http.NewRequest("GET", "/locations", nil)
+        Locations_Handler(recorder, req)
+        assert.NoError(t, err)
+        assert.Equal(t, 200, recorder.Code)
+        assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
+}
+
+func TestGetCategories(t *testing.T) {
+        t.Parallel()
+        recorder := httptest.NewRecorder()
+        req, err := http.NewRequest("GET", "/categories", nil)
+        Categories_Handler(recorder, req)
+        assert.NoError(t, err)
+        assert.Equal(t, 200, recorder.Code)
+        assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
+}
