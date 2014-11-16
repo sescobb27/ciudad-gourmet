7321228edeb381f7c420583455a7dfb5d903ba66
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-11-16 01:51:34 -0500

    Cookie expiration date validation and url escaping from session id, and async testing the changes

--- services/session/session_service.go
+++ services/session/session_service.go
@@ -36,7 +36,7 @@ func (cManager *CookieManager) SessionStart(res http.ResponseWriter, req *http.R
     var sessionStore SessionStore
     <-cManager.semaphore
     cookie, err := req.Cookie(cManager.cookieName)
-    if err != nil || cookie.Value == "" {
+    if err == http.ErrNoCookie || cookie.Value == "" {
         session_id, err := cManager.genSessionId()
         if err != nil {
             return nil, err
@@ -50,7 +50,17 @@ func (cManager *CookieManager) SessionStart(res http.ResponseWriter, req *http.R
         if err != nil {
             return nil, err
         }
-        sessionStore = cManager.provider.SessionRead(session_id)
+
+        if cookie.Expires.Before(time.Now()) {
+            cManager.provider.SessionDestroy(session_id)
+            session_id, err = cManager.genSessionId()
+            if err != nil {
+                return nil, err
+            }
+        } else {
+            sessionStore = cManager.provider.SessionRead(session_id)
+        }
+
         if sessionStore == nil {
             sessionStore = cManager.provider.SessionStore(session_id)
             cookie = cManager.genCookie(session_id)
@@ -85,15 +95,20 @@ func (cManager *CookieManager) genCookie(session_id string) *http.Cookie {
 }
 
 // Destroy session by its id in http request cookie.
-func (cManager *CookieManager) SessionDestroy(res http.ResponseWriter, req *http.Request) {
+func (cManager *CookieManager) SessionDestroy(res http.ResponseWriter, req *http.Request) error {
     <-cManager.semaphore
     cookie, err := req.Cookie(cManager.cookieName)
-    if err != nil || cookie.Value == "" {
-        return
-    } else {
-        cManager.provider.SessionDestroy(cookie.Value)
+    if err == nil && cookie.Value != "" {
+        session_id, err := url.QueryUnescape(cookie.Value)
+        if err != nil {
+            return err
+        }
+
+        cManager.provider.SessionDestroy(session_id)
         cookie.Expires = time.Now()
         http.SetCookie(res, cookie)
+        req.AddCookie(cookie)
     }
     cManager.semaphore <- signal{}
+    return nil
 }
--- services/session/session_service_test.go
+++ services/session/session_service_test.go
@@ -10,10 +10,21 @@ import (
     "time"
 )
 
-func TestSessionStart(t *testing.T) {
+// 2 requests at the same time should be treated as different so they should
+// return 2 different SessionStore values
+func TestSessionStart_2DifferentRequest(t *testing.T) {
     t.Parallel()
-    recorder := httptest.NewRecorder()
-    req, err := http.NewRequest(
+    // First simulated request and response
+    recorder1 := httptest.NewRecorder()
+    req1, err := http.NewRequest(
+        "POST",
+        "/signin",
+        strings.NewReader("username=sescob&password=qwerty"),
+    )
+
+    // Second simulated request and response
+    recorder2 := httptest.NewRecorder()
+    req2, err := http.NewRequest(
         "POST",
         "/signin",
         strings.NewReader("username=sescob&password=qwerty"),
@@ -29,25 +40,75 @@ func TestSessionStart(t *testing.T) {
         sessionStore2 SessionStore
     )
     ws.Add(1)
-    go func(cookie *CookieManager) {
+    go func(tmpCookie *CookieManager, res http.ResponseWriter, r *http.Request) {
         var err1 error
-        sessionStore, err1 = cookie.SessionStart(recorder, req)
+        sessionStore, err1 = tmpCookie.SessionStart(res, r)
         assert.NoError(t, err1)
         assert.NotNil(t, sessionStore)
-        assert.NotNil(t, recorder.HeaderMap.Get("Set-Cookie"))
+        cookieStr := res.Header().Get("Set-Cookie")
+        assert.True(t, strings.Contains(cookieStr, sessionStore.SessionID()))
+        assert.NotNil(t, res.Header().Get("Set-Cookie"))
+
         ws.Done()
-    }(cookie)
+    }(cookie, recorder1, req1)
     ws.Add(1)
-    go func(cookie *CookieManager) {
+    go func(tmpCookie *CookieManager, res http.ResponseWriter, r *http.Request) {
         var err2 error
-        sessionStore2, err2 = cookie.SessionStart(recorder, req)
+        sessionStore2, err2 = tmpCookie.SessionStart(res, r)
         assert.NoError(t, err2)
         assert.NotNil(t, sessionStore2)
-        assert.NotNil(t, recorder.HeaderMap.Get("Set-Cookie"))
+        cookieStr := res.Header().Get("Set-Cookie")
+        assert.True(t, strings.Contains(cookieStr, sessionStore2.SessionID()))
+        assert.NotNil(t, res.Header().Get("Set-Cookie"))
+
         ws.Done()
-    }(cookie)
+    }(cookie, recorder2, req2)
     ws.Wait()
     assert.NotNil(t, sessionStore)
     assert.NotNil(t, sessionStore2)
-    assert.Equal(t, sessionStore, sessionStore2)
+    assert.NotEqual(t, sessionStore, sessionStore2)
+}
+
+// 2 requests at the same time should be treated as different so the second one
+// can't read the session deleted by the previous one
+func TestSessionDestroy_2DifferentRequest(t *testing.T) {
+    t.Parallel()
+    recorder := httptest.NewRecorder()
+    req, err := http.NewRequest(
+        "POST",
+        "/signin",
+        strings.NewReader("username=sescob&password=qwerty"),
+    )
+    assert.NoError(t, err)
+    expiresOn := time.Now().AddDate(1, 0, 0) // expires 1 year after this one
+    provider := NewSessionProvider()
+    cookie := NewCookieManager("cookieTest", expiresOn, 16, provider)
+
+    var (
+        sessionStore SessionStore
+        ws           sync.WaitGroup
+    )
+    sessionStore, err = cookie.SessionStart(recorder, req)
+    assert.NoError(t, err)
+
+    ws.Add(1)
+    done := make(chan signal)
+    go func(rw http.ResponseWriter, r *http.Request) {
+        cookie.SessionDestroy(rw, r)
+        done <- signal{}
+        assert.NoError(t, err)
+        assert.Nil(t, cookie.provider.SessionRead(sessionStore.SessionID()))
+        ws.Done()
+    }(recorder, req)
+
+    ws.Add(1)
+    go func(rw http.ResponseWriter, r *http.Request) {
+        <-done
+        assert.Nil(t, cookie.provider.SessionRead(sessionStore.SessionID()))
+        cookie.SessionDestroy(rw, r)
+        assert.NoError(t, err)
+        assert.Nil(t, cookie.provider.SessionRead(sessionStore.SessionID()))
+        ws.Done()
+    }(recorder, req)
+    ws.Wait()
 }
